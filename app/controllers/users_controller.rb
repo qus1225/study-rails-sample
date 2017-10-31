da8f8bf443ba626e5e7f85873bece6f98cf5d46c
qus1225 <qus1225@gmail.com>
2017-10-31 17:17:58 +0900

    Add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def new
+    debuggeru
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/home', to: 'static_pages#home'
+
+  resources :users
 end
